c414ec6c514f3dfe554986343511ab056dfd108f
KvinPardo <kevin.pardov25@gmail.com>
2023-01-18 15:45:29 -0300

    dashboard-nextjs

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+    {
+      id: 1,
+      name: {
+        first: 'Kevin',
+        last: 'Pardo',
+      },
+      total: 2795.95,
+      status: 'On Hold',
+      method: 'PayPal',
+      date: '15 Minutes ago',
+    },
+    {
+      id: 2,
+      name: {
+        first: 'Camila',
+        last: 'Pardo',
+      },
+      total: 1195.95,
+      status: 'Processing',
+      method: 'PayPal',
+      date: '23 Minutes ago',
+    },
+    {
+      id: 3,
+      name: {
+        first: 'Patricia',
+        last: 'Veas',
+      },
+      total: 495.85,
+      status: 'Completed',
+      method: 'Visa',
+      date: '1 Hour ago',
+    },
+    {
+      id: 4,
+      name: {
+        first: 'Yerko',
+        last: 'Pardo',
+      },
+      total: 150.45,
+      status: 'Processing',
+      method: 'MasterCard',
+      date: '1 Hour ago',
+    },
+    {
+      id: 5,
+      name: {
+        first: 'Kevin',
+        last: 'Pardo',
+      },
+      total: 175.25,
+      status: 'On Hold',
+      method: 'PayPal',
+      date: '2 Hour ago',
+    },
+    {
+      id: 6,
+      name: {
+        first: 'Camila',
+        last: 'Pardo',
+      },
+      total: 1295.75,
+      status: 'Completed',
+      method: 'Check',
+      date: '3 Hour ago',
+    },
+    {
+      id: 7,
+      name: {
+        first: 'Yerko',
+        last: 'Pardo',
+      },
+      total: 89.95,
+      status: 'Completed',
+      method: 'MasterCard',
+      date: '3 Hour ago',
+    },
+    {
+      id: 8,
+      name: {
+        first: 'Patricia',
+        last: 'Veas',
+      },
+      total: 689.45,
+      status: 'Completed',
+      method: 'Visa',
+      date: '7 Hour ago',
+    },
+    {
+      id: 9,
+      name: {
+        first: 'Kevin',
+        last: 'Pardo',
+      },
+      total: 14.99,
+      status: 'Completed',
+      method: 'PayPal',
+      date: '1 Day ago',
+    },
+    {
+      id: 10,
+      name: {
+        first: 'Camila',
+        last: 'Pardo',
+      },
+      total: 218.99,
+      status: 'Completed',
+      method: 'MasterCard',
+      date: '1 Day ago',
+    },
+  ];
